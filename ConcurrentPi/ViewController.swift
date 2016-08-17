3bccf89f7071d485d1944fd9f1b60eeae7fb8f5d
Everett Kropf <ehkropf@gmail.com>
2016-08-17 08:59:03 +0100

    Connect steppers and labels.

--- ConcurrentPi/ViewController.swift
+++ ConcurrentPi/ViewController.swift
@@ -8,18 +8,70 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+extension UIStepper {
+    var intValue: Int { return Int(self.value) }
 }
 
+class ViewController: UIViewController {
+    
+    let trialsList = [Int(3.5e4), Int(1e5), Int(3.5e5), Int(1e6), Int(3.5e6), Int(1e7)]
+    var formatterTrials = NSNumberFormatter()
+    let jobsNumberList = [1, 2, 4, 8, 16]
+
+    @IBOutlet weak var labelTrials: UILabel?
+    @IBOutlet weak var stepperTrials: UIStepper?
+    
+    @IBOutlet weak var labelJobs: UILabel?
+    @IBOutlet weak var stepperJobs: UIStepper?
+    
+    //MARK: Lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        commonInit()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        commonInit()
+    }
+    
+    func commonInit() {
+        formatterTrials.numberStyle = .DecimalStyle
+        formatterTrials.usesGroupingSeparator = true
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        stepperTrials?.maximumValue = Double(trialsList.count - 1)
+        labelTrialsUpdate()
+        stepperJobs?.maximumValue = Double(jobsNumberList.count - 1)
+        labelJobsUpdate()
+    }
+
+    //MARK: Label setting
+    
+    func labelTrialsUpdate() {
+        guard let index = stepperTrials?.intValue else {
+            return
+        }
+        labelTrials?.text = formatterTrials.stringFromNumber(NSNumber(integer: trialsList[index]))
+    }
+    
+    func labelJobsUpdate() {
+        guard let index = stepperJobs?.intValue else {
+            return
+        }
+        labelJobs?.text = String(jobsNumberList[index])
+    }
+    
+    @IBAction func stepperTrialAction() {
+        labelTrialsUpdate()
+    }
+    
+    @IBAction func stepperJobsAction() {
+        labelJobsUpdate()
+    }
+    
+}
